fe16f496031659691d646c18bc6dd33b017f33fa
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-02-28 16:05:37 -0500

    Feature: starting project 😀

---
+++ src/pages/Formulario.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export function Formulario() {
+  return (
+    <Container>
+      <h1>Formulario</h1>
+    </Container>
+  );
+}
+const Container = styled.div`
+
+`;
